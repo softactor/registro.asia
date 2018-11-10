c1bf9aa82d0ddd3c4ea8495f0121a5d8b37217ab
softactor <tanveerqureshee1@gmail.com>
2018-11-10 08:57:01 +0600

    upload csv is now in helper method

--- app/Helpers/utilities.php
+++ app/Helpers/utilities.php
@@ -15,8 +15,6 @@ use QR_Code\QR_Code;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\URL;
-use Barryvdh\DomPDF\PDF;
-use Illuminate\Support\Facades\Mail;
 use PDF;
 use Mail;
 
