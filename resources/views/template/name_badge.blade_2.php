6353af6263d0c1db7a63afe813fc3ae5fb144efa
softactor <tanveerqureshee1@gmail.com>
2018-12-26 07:35:33 +0600

    bulk print on testing

--- resources/views/template/name_badge.blade_2.php
+++ resources/views/template/name_badge.blade_2.php
@@ -2,67 +2,9 @@
 <?php    
     $configFound    =   false;
     foreach($user_datas as $user_data){
-        $events = DB::table('events')->where('id', $user_data->event_id)->first();
         $whereData  =   [
-            'event_id'              =>  $user_data->event_id,
-            'namebadgeTemplateType' =>  'Default'
+            'event_id'=>$user_data->event_id
         ];
-        $getConfigNameBadgePos  =   get_data_name_by_where('name_badge_position', $whereData);
-        if(isset($getConfigNameBadgePos) && !empty($getConfigNameBadgePos)){
-            if($getConfigNameBadgePos->nameBadgeTemplateSet ==  "defaultEventNameBadge1"){ ?>
-                <style type="text/css">
-            @page {
-                        size: A4;
-                        margin: 0;
-                    }
-                body {
-                        width: 100%;
-                        height: 100%;
-                        margin: 0;
-                        padding: 0;
-                    }
-                    * {
-                        box-sizing: border-box;
-                        -moz-box-sizing: border-box;
-                    }
-                    @media print {
-                        html, body {
-                            width: <?php echo ((isset($printLayout->page_width) && !empty($printLayout->page_width))? $printLayout->page_width : '21cm') ?>;
-                            height: <?php echo ((isset($printLayout->page_height) && !empty($printLayout->page_height))? $printLayout->page_height : '9.7cm') ?>;
-                            margin-top: <?php echo ((isset($printLayout->margin_top) && !empty($printLayout->margin_top))? $printLayout->margin_top : '2cm') ?>;
-                            margin-right: <?php echo ((isset($printLayout->margin_right) && !empty($printLayout->margin_right))? $printLayout->margin_right : '0cm') ?>;
-                            margin-bottom: <?php echo ((isset($printLayout->margin_bottom) && !empty($printLayout->margin_bottom))? $printLayout->margin_bottom : '1.5cm') ?>;
-                            margin-left: <?php echo ((isset($printLayout->margin_left) && !empty($printLayout->margin_left))? $printLayout->margin_left : '1.5cm') ?>
-                        }                                
-                    }
-                    
-        </style>
-                    <div class="nameBadgeTemplateHolder">
-                        <div class="defaultEventNameBadge defaultEventNameBadge1">
-                            <div class="defaultEventNameBadgeheader">
-                                <img class="nameBadgeHeader img-responsive" src="<?php echo asset('/events/'.$events->event_header); ?>"/>
-                            </div>
-                            <div class="userBasicInfo">
-                                <h3><?php echo $user_data->first_name.' '.$user_data->last_name; ?></h3>
-                                <h4><?php echo $user_data->designation; ?></h4>
-                                <h5><?php echo $user_data->company_name ?></h5>
-                                <h6><?php echo $user_data->company_address; ?></h6>
-                            </div>
-                            <div class="userQrCode">
-                                <img src="<?php echo asset('pdf/'.$user_data->qrcode_path) ?>" />
-                            </div>
-                            <div class="userType">
-                                <h2><?php echo $user_data->namebadge_user_label; ?></h2>
-                            </div>
-                        </div>
-                    </div>
-            <?php }elseif($getConfigNameBadgePos->nameBadgeTemplateSet ==  "defaultEventNameBadge2"){
-                
-            }elseif($getConfigNameBadgePos->nameBadgeTemplateSet ==  "defaultEventNameBadge3"){
-                
-            }
-        }else{
-        
         $getConfig  =   get_data_name_by_where('name_badge_config', $whereData);
         if(isset($getConfig) && !empty($getConfig)){            
             $configFound = true;
@@ -174,7 +116,7 @@
     
     ?>
 </div>
-    <?php }}} ?>
+    <?php }} ?>
 <?php
     if(!$configFound){
         echo '<h1>Please configure the template first.</h1>';
